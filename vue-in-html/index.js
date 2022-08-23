4ee9eb0544720633dbd35808deb14e8bba24ed66
Guillermo Reyes Campos <guillermo.a.reyes.c@gmail.com>
2022-08-23 03:33:42 -0400

    vue in html

---
+++ vue-in-html/index.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    setup() {
+
+        const title = 'this is a title from vue3';
+        const name = 'minirick';
+        return {
+            title,
+            name,
+
+        };
+
+    }
+})
